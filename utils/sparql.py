8c344253e23f1810f34bf203e1ebf1f15de2764b
Stefan Oderbolz <oderbolz@gmail.com>
2024-11-14 00:03:30 +0100

    Add user-agent to SPARQL code

--- utils/sparql.py
+++ utils/sparql.py
@@ -2,7 +2,8 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 
 def query(query, endpoint="https://register.ld.admin.ch/query"):
-    sparql = SPARQLWrapper(endpoint)
+    user_agent = "Mozilla Firefox Mozilla/5.0; metaodi oneway-map"
+    sparql = SPARQLWrapper(endpoint, agent=user_agent)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.queryAndConvert()
