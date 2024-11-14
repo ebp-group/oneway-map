ebd1085676939fe5b6eb9176546024ca799014da
Stefan Oderbolz <oderbolz@gmail.com>
2024-11-14 17:07:07 +0100

    Pass full query

--- export_oneway_by_country.py
+++ export_oneway_by_country.py
@@ -33,14 +33,15 @@ for country in european_countries:
     if os.path.exists(gj_path):
         continue
     query = f"""
-    [timeout:600];
+    [out:json][timeout:600];
     rel["ISO3166-1:alpha2"="{country['isoCode']}"]["boundary"="administrative"];
     map_to_area;
     way["highway"!="motorway"]["highway"!="trunk"]["highway"!="primary_link"]["highway"!="motorway_link"]["highway"!="path"][!"lanes"][!"tramway"][!"railway"]["oneway"="yes"](area);
+    out body geom;
     """
     logging.debug(f" -> Query: {query}")
     try:
-        response = api.get(query, responseformat="geojson", verbosity="body geom")
+        response = api.get(query, responseformat="geojson", build=False)
     except overpass.OverpassError:
         logging.exception(f"Error from Overpass for {country['countryLabel']}")
         pause = input("**** PRESS ANY KEY TO CONTINUE ****")
