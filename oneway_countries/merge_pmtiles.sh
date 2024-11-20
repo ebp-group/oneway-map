2411c22483b9c3b68334b6ab1858d8304f01e8f5
Stefan Oderbolz <oderbolz@gmail.com>
2024-11-20 09:08:50 +0100

    Update merge_pmtiles.sh

--- oneway_countries/merge_pmtiles.sh
+++ oneway_countries/merge_pmtiles.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+set -e
+
+function cleanup {
+    exit $?
+}
+
+trap "cleanup" EXIT
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
 echo "Merge all PMTiles..."
 rm -f $DIR/merged.pmtiles
 tile-join --overzoom -z 17 -Z 5  -o $DIR/merged.pmtiles $DIR/*.pmtiles
